d989f07c82a841693cb06edc1548057a01c755cd
Jaison Erick <jaisonreis@gmail.com>
2024-04-13 13:46:43 -0300

    New keymappings

--- config/nvim/lua/mysetup/mappings.lua
+++ config/nvim/lua/mysetup/mappings.lua
@@ -3,17 +3,19 @@ vim.keymap.set('n', '<leader><leader>', '<C-^>', {desc = 'Switch between the las
 vim.keymap.set('i', 'jj', '<Esc>', {desc='Exit insert with jj'})
 
 vim.keymap.set('n', '<leader>-', '<cmd>wincmd _<cr><cmd>wincmd |<cr>', {desc='Zoom in current pane'})
-vim.keymap.set('n', '<leader>=', '<cmd>wincmd =<cr>', {desc='Zoom out current pane'})
-
-
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cmdline go to the beginning
+vim.keymap.set("c", "<C-A>", "<Home>")
 
 vim.keymap.set("x", "<leader>y", [["_dP]])
 
 vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float)
 
+vim.keymap.set('v', '<leader>jq', "<cmd>'<,'>! jq -M<cr><Esc>", { silent = true, desc = 'Format selected JSON' })
+vim.keymap.set('n', '<leader>jq', "<cmd>%!jq .<cr>", { silent = true, desc = 'Format JSON current buffer' })
 
 -- Pairs
 -- Diagnosics (g)
